6c5a4b2f6392c317b285ee0bdee1da71fba87bf8
harshit11purohit <harshitpurohit953@gmail.com>
2026-07-10 23:24:57 +0530

    chore: upgrade project dependencies and update environment gitignore rules

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Shriansh singh',
-    author_email= 'shrianshsingh@gmail.com',
+    author= 'Harshit Purohit',
+    author_email= 'harshitpurohit953@gmail.com',
     packages= find_packages(),
     install_requires = []
 
