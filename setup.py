49c6ba52b496a18fe49bb864466d59a8f56f3847
harshit11purohit <harshitpurohit953@gmail.com>
2026-07-13 20:12:41 +0530

    chore: update setup.py packaging config

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Medical Chatbot',
-    version= '0.0.0',
-    author= 'Harshit Purohit',
-    author_email= 'harshitpurohit953@gmail.com',
-    packages= find_packages(),
-    install_requires = []
-
+    name="MultiModal Clinical Intelligence Platform",
+    version="0.0.0",
+    author="Harshit Purohit",
+    author_email="harshitpurohit953@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
 )
